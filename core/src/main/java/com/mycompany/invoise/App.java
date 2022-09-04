1bccd8172bc3337b0b5f2fe54512fc9fffaca7e6
AhmedOg <ahmedogbinto@gmail.com>
2022-09-04 01:43:05 -0400

    Concept de conteneur leger spring : Spring lightweight container

--- core/src/main/java/com/mycompany/invoise/App.java
+++ core/src/main/java/com/mycompany/invoise/App.java
@@ -1,43 +1,24 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
-import com.mycompany.invoise.repository.InvoiceRepositoryInterface;
-import com.mycompany.invoise.service.InvoiceServiceInterface;
-
-import java.util.Scanner;
+import com.mycompany.invoise.controller.InvoiceControllerKeyboard;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Quel est la classe controlleur choisi?");
-        String controllerClass = scanner.nextLine();
-        System.out.println("Quel est la classe Service choisie?");
-        String serviceClass = scanner.nextLine();
-        System.out.println("Quel est la classe repositorie choisie?");
-        String repositoryClass= scanner.nextLine();
-
-        InvoiceControllerInterface invoiceControllerInterface = null;
-        InvoiceServiceInterface invoiceServiceInterface = null;
-        InvoiceRepositoryInterface invoiceRepositoryInterface = null;
-
-        try{
-            invoiceControllerInterface = (InvoiceControllerInterface) Class.forName(controllerClass).getDeclaredConstructor().newInstance();
-            invoiceServiceInterface = (InvoiceServiceInterface) Class.forName(serviceClass).getDeclaredConstructor().newInstance();
-            invoiceRepositoryInterface = (InvoiceRepositoryInterface) Class.forName(repositoryClass).getDeclaredConstructor().newInstance();
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-// injection des dependences
-        invoiceControllerInterface.setInvoiceServiceInterface(invoiceServiceInterface);
-        invoiceServiceInterface.setInvoiceRepositoryInterface(invoiceRepositoryInterface);
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class);
 
         invoiceControllerInterface.createInvoiceUsing();
+
+
     }
+
 }
+
