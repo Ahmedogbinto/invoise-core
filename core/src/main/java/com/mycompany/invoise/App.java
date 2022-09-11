7eaf8d25df60a375d590ec2dfd3da3be38c85cac
AhmedOg <ahmedogbinto@gmail.com>
2022-09-11 03:28:04 -0400

    Les classes de configuration AppConfig et AppConfigPropertySource plus en details partie 2

--- core/src/main/java/com/mycompany/invoise/App.java
+++ core/src/main/java/com/mycompany/invoise/App.java
@@ -1,9 +1,10 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.service.InvoiceServiceInterface;
+import com.mycompany.invoise.service.prefix.InvoiceServicePrefix;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 
 /**
@@ -11,18 +12,22 @@ import org.springframework.context.annotation.Configuration;
  *
  */
 @Configuration
+@ComponentScan(basePackages = {"com.mycompany.invoise.repository.database",/*"com.mycompany.invoise.service.prefix",*/"com.mycompany.invoise.controller.web"})
+@PropertySource("classpath:application.properties")
 public class App {
     public static void main(String[] args) {
 
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");           // Utilisation du fichier applicationContext.xml
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class, AppConfigPropertySource.class);                  // Utilisation de la classe de configuratin AppConfig
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);                  // Utilisation de la classe de configuratin AppConfig
         InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class);
 
         invoiceControllerInterface.createInvoiceUsing();
-
-
     }
 
+    @Bean
+    public InvoiceServiceInterface configureInvoiceService(){
+        return new InvoiceServicePrefix();
+    }
 }
 
