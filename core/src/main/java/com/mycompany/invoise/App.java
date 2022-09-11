7d8a7bd1926d33f8130a1069a4fe3307f89f4a88
AhmedOg <ahmedogbinto@gmail.com>
2022-09-11 02:48:34 -0400

    Concepte: Les classes de configuration avec plus de détails partie 1

--- core/src/main/java/com/mycompany/invoise/App.java
+++ core/src/main/java/com/mycompany/invoise/App.java
@@ -3,18 +3,20 @@ package com.mycompany.invoise;
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
 
 /**
  * Hello world!
  *
  */
+@Configuration
 public class App {
     public static void main(String[] args) {
 
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");           // Utilisation du fichier applicationContext.xml
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);                  // Utilisation de la classe de configuratin AppConfig
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class, AppConfigPropertySource.class);                  // Utilisation de la classe de configuratin AppConfig
         InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class);
 
         invoiceControllerInterface.createInvoiceUsing();
---
+++ core/src/main/java/com/mycompany/invoise/AppConfigPropertySource.java
@@ -0,0 +1,10 @@
+package com.mycompany.invoise;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AppConfigPropertySource {
+}
