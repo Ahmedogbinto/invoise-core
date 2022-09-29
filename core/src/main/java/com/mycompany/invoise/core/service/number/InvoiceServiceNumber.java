903b6e5cdb919f57982c42bb317f3bde20ec731f
AhmedOg <ahmedogbinto@gmail.com>
2022-09-28 20:29:00 -0400

    Developper une QPI ReST qvec Spring: ResponseBody - RequestBody et RestController

--- core/src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
+++ core/src/main/java/com/mycompany/invoise/core/service/number/InvoiceServiceNumber.java
@@ -22,12 +22,13 @@ public class InvoiceServiceNumber implements InvoiceServiceInterface {
 
     private static long lastNumber = 0L;
 
-    public void createInvoice(Invoice invoice){
+    public Invoice createInvoice(Invoice invoice){
         invoice.setNumber(String.valueOf(++lastNumber));
 
         invoiceRepository.create(invoice);
 
         System.out.println("la facture a bien été ajouté, le nom du client c'est "+invoice.getCustomerName()+" et le numero de facture est "+invoice.getNumber());
+    return invoice;
     }
 
     @Override
--- core/src/main/java/com/mycompany/invoise/core/service/prefix/InvoiceServicePrefix.java
+++ core/src/main/java/com/mycompany/invoise/core/service/prefix/InvoiceServicePrefix.java
@@ -42,12 +42,13 @@ import java.util.List;
         this.prefix = prefix;
     }
 
-    public void createInvoice(Invoice invoice){
+    public Invoice createInvoice(Invoice invoice){
         invoice.setNumber(String.valueOf(prefix+(++lastNumber)));
 
         invoiceRepository.create(invoice);
 
         System.out.println("la facture a bien été ajouté, le nom du client c'est "+invoice.getCustomerName()+" et le numero de facture est "+invoice.getNumber());
+    return invoice;
     }
 
     @Override
